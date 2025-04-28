bd93329980163be507986c61aa91698d1c73ad98
gansteroflove-arch <reesie1998@gmail.com>
2025-04-28 00:32:58 -0400

    update dashboard-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {HashRouter, Routes, Route} from "react-router-dom"
+import {HashRouter, Routes, Route} from "react-router-dom";
 import Home from "./components/home";
 import Login from "./components/ Login";
 import Register from "./components/ Register";
